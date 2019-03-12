490c932920e86023957df5d353af8f15d850464b
Aleksandar Antonov <alekzandar@live.com>
2019-03-12 18:34:29 -0400

    Large refactor for DRY - navbar and footer redundancy removed

---
+++ SignUp/Scripts/footer.js
@@ -0,0 +1,54 @@
+document.write('\
+<style>\
+\
+footer {\
+    position: fixed;\
+    background-color:papayawhip;\
+    text-align: center;\
+    padding: 5px;\
+    color: rgb(241, 160, 8);\
+  }\
+  footer li {\
+    list-style-type: none;\
+    display: inline;\
+    margin-right: 30px;\
+  }\
+  </style>\
+\
+<!-- Footer-->\
+<footer class="page-footer font-small unique-color-dark pt-4 fixed-bottom">\
+    <div class="container-fluid padding">\
+\
+        <!-- Footer Elements -->\
+        <ul class="list-unstyled list-inline text-center">\
+            <li class="list-inline-item">\
+                <a class="btn-floating btn-fb mx-1" href="https://www.facebook.com/revature/">\
+                    <em class="fab fa-facebook-f"> </em>\
+                </a>\
+            </li>\
+            <li class="list-inline-item">\
+                <a class="btn-floating btn-tw mx-1"\
+                    href="https://twitter.com/WeAreRevature?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor">\
+                    <em class="fab fa-twitter"> </em>\
+                </a>\
+            </li>\
+            <li class="list-inline-item">\
+                <a class="btn-floating btn-li mx-1" href="https://www.linkedin.com/company/revature/">\
+                    <em class="fab fa-linkedin-in"> </em>\
+                </a>\
+            </li>\
+            <li class="list-inline-item">\
+                <a class="btn-floating btn-li mx-1" href="https://www.instagram.com/teamrevature/">\
+                    <em class="fab fa-instagram "> </em>\
+                </a>\
+            </li>\
+            </li>\
+        </ul>\
+    </div>\
+\
+    <div class="footer-copyright text-center py-3">© 2019 Copyright:\
+        <a href="**"> Revature.com</a>\
+    </div>\
+    </footer>\
+\
+');
---
+++ SignUp/Scripts/navbar.js
@@ -0,0 +1,16 @@
+document.write('\
+\
+<nav class="navbar navbar-expand-lg navbar-light stick-top" style="background-color: #e3f2fd;">\
+<a class="navbar-brand" href="https://revature.com/"><img src="./Media/revlogo.png" alt="RevImg" width="18%">\
+    Sign-Up Today!</a>\
+<div class="collapse navbar-collapse" id="navbarNavAltMarkup">\
+    <div class="navbar-nav">\
+        <a class="nav-item nav-link active" href="intro.html">Home <span class="sr-only">(current)</span></a>\
+        <a class="nav-item nav-link" href="SignUp.html">Sign-Up</a>\
+        <a class="nav-item nav-link" href="Contact.html">Contact Us</a>\
+        <a class="nav-item nav-link" id="logIn" href="logIn.html">Log In</a>\
+    </div>\
+</div>\
+</nav>\
+\
+');
