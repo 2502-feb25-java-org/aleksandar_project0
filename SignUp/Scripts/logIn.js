ec2a98662fafd4d6434805ce2f11e722f5e3e0f5
Aleksandar Antonov <alekzandar@live.com>
2019-03-13 12:06:51 -0400

    Added 'Remember me' for username in LogIn

--- SignUp/Scripts/logIn.js
+++ SignUp/Scripts/logIn.js
@@ -2,25 +2,38 @@
 const correctUser = "secretsnake";
 const correctPassword = "revhero";
 
-function confirmLogIn(){
-    let username = document.getElementById('login').value;
-    let password = document.getElementById('password').value;
-    if (username == correctUser && password == correctPassword){
-        myFunction();
-        window.location.href="snakeGame.html"; 
-    } else {
-        //console.log("Incorrect");
-        alert ('Please try again. \nHINT: Forgot your password?');
-    }
-    return false;
+function confirmLogIn() {
+  let username = document.getElementById('login').value;
+  let password = document.getElementById('password').value;
+  if (username == correctUser && password == correctPassword) {
+    myFunction();
+    window.location.href = "snakeGame.html";
+  } else {
+    //console.log("Incorrect");
+    alert('Please try again. \nHINT: Forgot your password?');
+  }
+  return false;
 }
 
+function handleChange(checkbox) {
+  if (checkbox.checked == true) {
+    let id = event.target.id; //event wasn't available in firefox
+    let username = document.getElementById("login").value;
+    localStorage.setItem(id, username);
+  }
+}
+
+function getUser(){
+  let element = document.getElementById("login");
+  let value = localStorage.getItem("remember");
+  user.value = value;
+}
 
 function myFunction() {
-    var x = document.getElementById("logIn");
-    if (x.style.display === "none") {
-      x.style.display = "block";
-    } else {
-      x.style.display = "none";
-    }
+  var x = document.getElementById("logIn");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
   }
+}
